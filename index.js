f6a302419eb7ed7b901070091117bd42ae00492b
AsrofurRizqi <asrofurrizqi@gmail.com>
2023-08-26 19:46:11 +0700

    revert https

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 require("dotenv").config({});
 const cors = require("cors");
-const fs = require('fs');
 const express = require("express");
-const https = require("https");
 const methodOverride = require("method-override");
 const cookieParser = require("cookie-parser");
 const app = express();
-const key = fs.readFileSync(__dirname + '/certs/kuro.key');
-const cert = fs.readFileSync(__dirname + '/certs/kuro.crt');
-
-const credentials = {
-  key: key,
-  cert: cert
-};
 const port = process.env.PORT || 3000;
 
 app.use(cors());
@@ -25,9 +16,7 @@ app.use(methodOverride("_method"));
 
 app.use("/api", mainRoutes);
 
-const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(port, '127.0.0.1', () => {
+app.listen(port, '127.0.0.1', () => {
     console.log(`Server running on port ${port}`);
     }
 );
